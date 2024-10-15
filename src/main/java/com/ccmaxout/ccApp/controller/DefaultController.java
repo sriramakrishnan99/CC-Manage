5eb6eaa0c0fcb443e37b3d4df3ce575c4fe20956
sriramakrishnanK <ksriramakrishnan99@gmail.com>
2024-10-15 21:21:05 +0530

    added some changes in default controller

--- src/main/java/com/ccmaxout/ccApp/controller/DefaultController.java
+++ src/main/java/com/ccmaxout/ccApp/controller/DefaultController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome(){
         return "{ \"application-name\" : \"CC-MaxOut\" }";
     }
